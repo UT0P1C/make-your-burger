f30a02ddcdc9d0ac47b4cf3f53586a2cdc3bfc94
UT0P1C <ljonham@gmail.com>
2022-02-08 13:01:08 -0300

    adicionando o banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div class="main-banner">
+        <h1>Make your burger</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+
+<style scoped>
+
+.main-banner{
+    background-image: url('/img/burger.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+.main-banner h1{
+    color: #fff;
+    background-color: #222;
+    font-size: 60px;
+    padding: 20px 40px;
+}
+
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
+
     <footer id="footer">
+        
         <p>Make your burger &copy; 2022</p>
+        
+        <a href="https://twitter.com/esq1z0" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" id="logo"/></a>
+
+        <a href="https://github.com/UT0P1C" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" id="logo"/></a>
+
     </footer>
+
 </template>
 
 <script>
@@ -23,6 +31,12 @@ export default {
     justify-content: center;
     width: 100%;
 
+}
+
+#logo{
+    width: 30px;
+    height: 20px;
+    margin: 5px;
 }
 
 </style>
